5dbc8fdc6de3ec0f16af4d1b388c57b2c027889f
niazmurshedsiam <niazmurshed16@gmail.com>
2021-02-27 20:59:38 +0600

    pass data state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <Nayok name="Shuvo"></Nayok>
         <Nayok name="Siam"></Nayok>
         <Nayok name="Sakib"></Nayok>
+        <MovieCounter></MovieCounter>
+        <Product></Product>
+        
       </header>
     </div>
   );
+}
+function MovieCounter(){
+  const [count,setCount] = useState(0);
+  const handleClick = ()=> setCount(count + 1);
+  
+  // console.log(count,setCount);
+  return(
+    <div>
+      <button onClick={handleClick}>Add Movie</button>
+      <h1>Nunber of movie : {count}</h1>
+      <Display display={count}></Display>
+    </div>
+  )
+}
+function Display(props){
+  return(
+    <h1>Display : {props.display}</h1>
+  )
+}
+
+
+function Product(){
+  const [count,setCount] = useState(0);
+  const handleAddProduct = () => setCount(count + 1);
+  const handleSubProduct = () => setCount(count - 1);
+
+  return(
+    <div>
+      <button onClick={handleAddProduct}>Add Product</button>
+      <button onClick={handleSubProduct}>Sub Product</button>
+      <h1>Number of Product : {count}</h1>
+      
+    </div>
+  )
 }
 function Nayok(props) {
   console.log(props);
