a7bf7eb8d3c97277de2b754e0093d799fcd3d9d1
niazmurshedsiam <niazmurshed16@gmail.com>
2021-02-27 14:53:35 +0600

    pass data through props

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Nayok></Nayok>
+        <Nayok name="Shuvo"></Nayok>
+        <Nayok name="Siam"></Nayok>
+        <Nayok name="Sakib"></Nayok>
       </header>
     </div>
   );
 }
-function Nayok() {
-  return <h1>Hello</h1>
+function Nayok(props) {
+  console.log(props);
+  return (
+    <div>
+      <h1>Ami Nayok {props.name}</h1>
+      <p>I have done 5 movie</p>
+    </div>
+  );
   
 }
 
